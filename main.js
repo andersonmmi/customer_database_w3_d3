25b45be631b2e33118351e3e561efeabd7331f50
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-02 16:56:25 -0400

    Titles removed, styles added.

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ function buildDirectory(){
         buildArea.appendChild(div);
         let content = `
             <img src=${customers.results[i].picture.large} alt="Image">
-            <h2 class="name">${customers.results[i].name.title} ${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
+            <h2 class="name">${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
             <h3 class="email">${customers.results[i].email}</h3>
             <h4 class="street_address">${customers.results[i].location.street}</h4>
             <h4 class="location">${customers.results[i].location.city}, ${customers.results[i].location.state} ${customers.results[i].location.postcode}</h4>
             <h4 class="phone">${customers.results[i].phone}</h4>
-            <h5 class="social">test</h5>
+            <h5 class="social">${customers.results[i].id.value}</h5>
         `;
 
     div.innerHTML = content;
