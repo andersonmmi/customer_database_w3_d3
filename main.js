8061afa7f195f41f4aad02e1b3d769ba91386598
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-02 15:53:39 -0400

    Emails and Street address populate.

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function buildDirectory(){
         let content = `
             <img src=${customers.results[i].picture.thumbnail} alt="Image">
             <h2 class="name">${customers.results[i].name.title} ${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
-            <h3 class="email">test</h3>
-            <h4 class="street_address">test</h4>
+            <h3 class="email">${customers.results[i].email}</h3>
+            <h4 class="street_address">${customers.results[i].location.street}</h4>
             <h4 class="location">test</h4>
             <h4 class="phone">test</h4>
             <h5 class="social">test</h5>
